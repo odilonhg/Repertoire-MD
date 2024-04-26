409dba6b6c1b89f139a874a550082a00bdf81af4
odilonhg <168239755+odilonhg@users.noreply.github.com>
2024-04-27 01:43:33 +0200

    Version 1.0
    Ici, vous pourrez faire de nombreuses choses qu'un répertoire permet de faire, notamment le détruire !
    
    	-> Contenu du Repertoire MD (V1) :
    
    		-fonction choix() -> C'est d'ici que débute ton expérience sur le répertoire ! Tu peux y faire tous les choix amenant à faire évoluer ton répertoire.
    		
    		-foncion ajoute() -> C'est ici que tu vas créer ton premier contact (et tous les prochains aussi) ! Dans la V1, les contacts sont enregistrés comme ceci : repertoire[cle] = num (tu enregistres tom nom, prenom et numéro de téléphone.
    		
    		-fonction recherche() -> C'est avec cette fonction que tu pourras retrouver un contact précis dans ta liste de 10056 contacts ! Tu peux le rechercher par son nom ou par son prénom.
    		
    		-fonction affiche() -> Cette fonction est chargée d'afficher l'ensemble de tes contacts
    		
    		-fonction ajoute_fav() -> C'est ici que tu ajoutes un de tes contacts en favori (permettant de le retrouver plus facilement par la suite !).
    		
    		-fonction affiche_fav() -> C'est lui qui affiche uniquement tes contacts favoris.
    		
    		-fonction supprimer_fav() -> Lui, il SUPPRIME un de tes contacts favoris (il redevient un simple contact).
    		
    		-fonction supprimer() -> Lui, encore pire, il SUPPRIME un de tes contacts (mais si il est dans le répertoire favori il reste sauvegarder dedans parce que la V1 est mal faite).
    		
    		-fonction modifier() -> C'est ici que tu pourras modifier le numéro de téléphone d'un de tes contacts (si jamais il change de numéro !).
    		
    		-fonction options() -> C'est la suite de choix(), avec des fonctions moins utiles comme celles des favoris ou encore l'option spéciale
    		
    		-fonction options_super_mega_speciales_ultra_3000() -> Franchement, moi et Maxime étions obligés de mettre une pincée d'identité dans notre répertoire, la voici !
    			Cette fonction a pour unique objectif de te faire dire "oui" 20 fois pour confirmer que tu veux accéder à l'option spéciale ( la_fin() )
    		
    		-fonction la_fin() -> C'est la fin, tu t'es amusé avec notre répertoire, tu l'as testé et tu as croisé 10056 bugs d'affichage, tu en as assez...
    			La fonction la_fin() te permet d'en finir avec ce chapitre honteux de ta vie : Il efface le fichier repertoire, empêchant le fonctionnant du répertoire à tout jamais !

--- PROJET_repertoire.py
+++ PROJET_repertoire.py
@@ -1,43 +1,42 @@
+#ATTENTION, NE PAS PORTER D'ATTENTION PARTICULIERE AUX FONCTIONS la_fin() et la fameuse options_super_mega_speciales_ultra_3000() avant de les avoir essayées !
+
 from pickle import *
 
 def ajoute():
-    rep = open ('MD_repertoire','rb')
-    repertoire = load(rep)
+    rep = open ('MD_repertoire', 'rb')
+    repertoire = load (rep)
     rep.close()
     
     nom = input ('\nSaisir le nom du contact : ').upper()
     prenom = input ('Saisir le prénom du contact : ').capitalize()
-    cle = (nom,prenom)
-
+    cle = (nom, prenom)
     num = input ('Saisir le numéro de téléphone du contact : ')
     repertoire[cle] = num
     
-    rep = open ('MD_repertoire','wb')
-    dump (repertoire,rep)
+    rep = open ('MD_repertoire', 'wb')
+    dump (repertoire, rep)
     rep.close()
     return choix()
 
 def recherche():
-    rep = open ("MD_repertoire",'rb')
-    dico_recherche = load(rep)
+    rep = open ("MD_repertoire", 'rb')
+    dico_recherche = load (rep)
     rep.close()
-#     print(dico_recherche)
     contact_recherche = input ('\nDonnez le nom ou le prénom du contact : ')
     for name in dico_recherche.keys():
-#         print(name)
         if contact_recherche.lower() == name[0].lower():
-            print('')
-            print(name, ':', dico_recherche[name])
-            print('')
+            print ('')
+            print (name, ':', dico_recherche[name])
+            print ('')
         elif contact_recherche.lower() == name[1].lower():
-            print('')
-            print(name, ':', dico_recherche[name])
-            print('')
+            print ('')
+            print (name, ':', dico_recherche[name])
+            print ('')
     return choix()    
 
 def affiche():
-    rep = open ("MD_repertoire",'rb')
-    repertoire = load(rep)
+    rep = open ("MD_repertoire", 'rb')
+    repertoire = load (rep)
     rep.close()
     
     if len (repertoire.keys()) >= 1:
@@ -45,17 +44,17 @@ def affiche():
         for name, num in repertoire.items():
             print (f'{name} : {num}')
     else:
-        return print ('\nAucun contact trouvé.'),choix()
+        return print ('\nAucun contact trouvé.'), choix()
         
     return choix()
 
 def ajoute_fav():
     rep = open ("MD_repertoire", 'rb')
-    repertoire = load(rep)
+    repertoire = load (rep)
     rep.close()
-    print('')
+    print ('')
     rep_fav = open ("MD_repertoire_fav", "rb")
-    repertoire_fav = load(rep_fav)
+    repertoire_fav = load (rep_fav)
     rep_fav.close()
 
     contact_recherche = input ('Donnez le nom ou le prénom du contact à ajouter aux favoris : ')
@@ -63,61 +62,61 @@ def ajoute_fav():
     for name in repertoire.keys():
         if contact_recherche.lower() == name[0].lower():
             if name in repertoire_fav.keys():
-                return print ('\nContact déjà en favori'),options()
+                return print ('\nContact déjà en favori'), options()
             repertoire_fav[name] = repertoire[name]
-            rep_fav = open ("MD_repertoire_fav", "wb")
-            dump (repertoire_fav,rep_fav)
+            rep_fav = open("MD_repertoire_fav", "wb")
+            dump (repertoire_fav, rep_fav)
             rep_fav.close()
-            return print (f'\nLe contact {name} a été ajouté aux favoris !'),options()
+            return print (f'\nLe contact {name} a été ajouté aux favoris !'), options()
         elif contact_recherche.lower() == name[1].lower():
             if name in repertoire_fav.keys():
-                return print ('\nContact déjà en favori'),options()
+                return print ('\nContact déjà en favori'), options()
             repertoire_fav[name] = repertoire[name]
             rep_fav = open ("MD_repertoire_fav", "wb")
-            dump (repertoire_fav,rep_fav)
+            dump (repertoire_fav, rep_fav)
             rep_fav.close()
-            return print (f'\nLe contact {name} a été ajouté aux favoris !'),options()
+            return print (f'\nLe contact {name} a été ajouté aux favoris !'), options()
     
-    return print ('\nContact inexistant !'),options()
+    return print ('\nContact inexistant !'), options()
 
 def affiche_fav():
-    rep = open ("MD_repertoire_fav",'rb')
-    repertoire = load(rep)
+    rep = open ("MD_repertoire_fav", 'rb')
+    repertoire = load (rep)
     rep.close()
     
-    if len (repertoire.keys()) >= 1:
+    if len(repertoire.keys()) >= 1:
         print ('\nListe des contacts favoris :')
         for name, num in repertoire.items():
             print (f'{name} : {num}')
     else:
-        return print ('\nAucun contact ajouté en favori.'),options()
+        return print('\nAucun contact ajouté en favori.'),options()
         
     return options()
 
 def supprimer_fav():
-    rep_fav = open ("MD_repertoire_fav", "rb")
+    rep_fav = open("MD_repertoire_fav", "rb")
     repertoire_fav = load(rep_fav)
     rep_fav.close()
     print('')
-    contact_recherche = input ('Donnez le nom ou le prénom du contact à supprimer de vos favoris : ')
+    contact_recherche = input('Donnez le nom ou le prénom du contact à supprimer de vos favoris : ')
     
     for name in repertoire_fav.keys():
-        if contact_recherche.lower() == name[0].lower():
+        if contact_recherche.lower()==name[0].lower():
             if name in repertoire_fav.keys():
                 del repertoire_fav[name]
-                rep_fav = open ("MD_repertoire_fav", "wb")
+                rep_fav = open("MD_repertoire_fav", "wb")
                 dump(repertoire_fav,rep_fav)
                 rep_fav.close()
-                return print (f'\nLe contact {name} à été retiré des favoris !'),options()
-        elif contact_recherche.lower() == name[1].lower():
+                return print(f'\nLe contact {name} à été retiré des favoris !'),options()
+        elif contact_recherche.lower()==name[1].lower():
             if name in repertoire_fav.keys():
                 del repertoire_fav[name]
-                rep_fav = open ("MD_repertoire_fav", "wb")
-                dump (repertoire_fav,rep_fav)
+                rep_fav = open("MD_repertoire_fav", "wb")
+                dump(repertoire_fav,rep_fav)
                 rep_fav.close()
-                return print (f'\nLe contact {name} à été retiré des favoris !'),options()
+                return print(f'\nLe contact {name} à été retiré des favoris !'),options()
             
-    return print ("\nLe contact est inexistant ou n'est pas dans les favoris"),options()
+    return print("\nLe contact est inexistant ou n'est pas dans les favoris"),options()
 
 def supprimer():
     print ('\nATTENTION, cette action est irréversible !')
@@ -126,9 +125,9 @@ def supprimer():
         avertissement = input ('\nRéponse invalide. Veuillez taper "oui" pour confirmer ou "non" pour annuler : ')
     if avertissement == 'non':
         return print ('\nAction annulée'),options()
-    elif avertissement == 'oui':
-        rep = open ('MD_repertoire','rb')
-        repertoire = load(rep)
+    elif avertissement=='oui':
+        rep = open ('MD_repertoire', 'rb')
+        repertoire = load (rep)
         rep.close()
     
         contact = input ('\nDonnez le nom ou le prénom du contact à supprimer : ')
@@ -136,7 +135,7 @@ def supprimer():
             if contact.lower() == name[0].lower():
                 if name in repertoire.keys():
                     del repertoire[name]
-                    rep = open ('MD_repertoire','wb')
+                    rep = open ('MD_repertoire', 'wb')
                     dump (repertoire,rep)
                     rep.close()
                     return print (f'\nLe contact {name} a été supprimé !'),options()
@@ -150,33 +149,78 @@ def supprimer():
         return print ("\nLe contact est inexistant !"),options()
 
 def modifier():
-    rep = open ('MD_repertoire','rb')
-    repertoire = load(rep)
+    rep = open ('MD_repertoire', 'rb')
+    repertoire = load (rep)
     rep.close()
+    rep_fav = open ('MD_repertoire_fav', 'rb')
+    repertoire_fav = load (rep_fav)
+    rep_fav.close()
     
-    print('')
+    print ('')
     contact = input ('Donnez le nom ou le prénom du contact à modifier : ')
+    nouveau_num = input (f'Entrez un nouveau numéro de téléphone pour {contact} : ')
     for name in repertoire.keys():
         if contact.lower() == name[0].lower():
             if name in repertoire.keys():
-                print('')
-                nouveau_num = input (f'Entrez un nouveau numéro de téléphone pour {name} : ')
-                repertoire[name]=nouveau_num
-                rep = open ('MD_repertoire','wb')
-                dump(repertoire,rep)
+                for name_fav in repertoire_fav.keys():
+                    if contact.lower() == name[0].lower():
+                        if name in repertoire_fav.keys():
+                              repertoire_fav[name] = nouveau_num
+                              rep_fav = open ('MD_repertoire_fav', 'wb')
+                              dump (repertoire_fav, rep_fav)
+                              rep_fav.close()
+                repertoire[name] = nouveau_num
+                rep = open ('MD_repertoire', 'wb')
+                dump (repertoire,rep)
                 rep.close()
                 return print ('\nLe numéro du contact a été modifié !'),options()
         elif contact.lower() == name[1].lower():
             if name in repertoire.keys():
-                print('')
-                nouveau_num = input (f'Entrez un nouveau numéro de téléphone pour {name} : ')
+                for name_fav in repertoire_fav.keys():
+                    if contact.lower() == name[0].lower():
+                        if name in repertoire_fav.keys():
+                              repertoire_fav[name] = nouveau_num
+                              rep_fav = open ('MD_repertoire_fav', 'wb')
+                              dump (repertoire_fav,rep_fav)
+                              rep_fav.close()
                 repertoire[name] = nouveau_num
-                rep = open ('MD_repertoire','wb')
+                rep = open('MD_repertoire', 'wb')
                 dump (repertoire,rep)
                 rep.close()
                 return print ('\nLe numéro du contact a été modifié !'),options()
+                     
     return print ('\nContact inexistant'),options()
-                
+      
+def la_fin():
+    repertoire = {}
+    contact1 = ('MESNAGE', 'Dylan')
+    num1 = '0665218476'
+    contact2 = ('GORLAS', 'Maxime')
+    num2 = '0756245841'
+    repertoire[contact1] = num1
+    repertoire[contact2] = num2
+    rep = open ('MD_repertoire','wb')
+    dump (repertoire, rep)
+    rep.close()
+    rep_fav = open ('MD_repertoire_fav','wb')
+    dump (repertoire, rep_fav)
+    rep_fav.close()
+    
+    print ('\nLes Crédits :')
+    print ('')
+    print ('\nLes Créateurs')
+    print ('\nGORLAS Maxime')
+    print ('MESNAGE Dylan')
+    print ('')
+    print ('\nLes Cerveaux')
+    print ('\nLEMERCIER Guénaël')
+    print ('Mr MOUCHEL')
+    print ('')
+    print ('\nRemerciement Spécial :')
+    print ('\nMr MOUCHEL')
+    print ('\nMerci d\'avoir essayé notre programme, maintenant, tous les repertoires ont été effacé !')
+    return ''
+     
 def choix():
     print ('\n1-Rechercher un contact')
     print ('2-Ajouter un contact')
@@ -205,6 +249,7 @@ def options():
     print ('4-Modifier un contact')
     print ('5-Supprimer un contact')
     print ('6-Retour')
+    print('7-Options SUPER MEGA Spéciales ULTRA 3000')
     choice=input('Choix : ')
     if choice == "6":
         return choix()
@@ -218,7 +263,51 @@ def options():
         return modifier()
     elif choice == '5':
         return supprimer()
+    elif choice=='7':
+        return options_super_mega_speciales_ultra_3000()
     else:
         print ('\nChoix impossible\n')
         return options()
+
+def options_super_mega_speciales_ultra_3000():
+    print ('\nATTENTION, cette action est irréversible !!!')
+    avert = input ('\nTapez "oui" pour confirmer ou "non" pour annuler : ')
+    if avert == 'oui':
+        avert2 = input ('\nEtes-vous sûr de continuer ??? : ')
+        if avert2 == 'oui':
+            avert3 = input ("\nDernière chance, si tu es sûr, tape 'je confirme sur l'honneur que je suis sûr de vouloir continuer' : ")
+            if avert3 == "je confirme sur l'honneur que je suis sûr de vouloir continuer":
+                avert4 = input ("\nToute dernière chance, tout peut s'arrêter ici, là maintenant, tu en as bien conscience ? (Si tu veux continuer, tape 'oui' : ")
+                if avert4 == 'oui':
+                    print ('\nAu revoir...')
+                    return la_fin()
+                elif avert4 == 'non':
+                    print ('\nBalourd...')
+                    return choix()
+                else:
+                    print ('\nAction Impossible')
+                    return options_super_mega_speciales_ultra_3000()
+            elif avert3 == 'non':
+                print('\nPourquoi faire tout cela pour annuler ???')
+                return choix()
+            else:
+                print ('\nTu écris trop vite je crois... courage !')
+                return options_super_mega_speciales_ultra_3000()
+        elif avert2 == 'non':
+            print ('\nAction annulée, dommage...')
+            return choix()
+        else:
+            print ('\nAction Impossible')
+            return options_super_mega_speciales_ultra_3000()
+    elif avert == 'non':
+        print ('\nAction annulée')
+        return choix()
+    else:
+        print ('\nAction impossible')
+        return options_super_mega_speciales_ultra_3000()
+
 choix()
+
+# variable = input ('Pressez "espace"')
+# if variable.issplace():
+# 	pass
